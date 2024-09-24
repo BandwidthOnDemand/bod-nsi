31b80645a3939f797ba6c58adea39a55aa6d55c1
Erik Rozendaal <erik@deler.org>
2024-09-24 10:05:50 +0200

    Use single wsimport command to generate all WSDL/XSD based sources

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 open module bod.nsi {
 	requires transitive java.xml;
 	requires transitive jakarta.xml.bind;
+	requires transitive jakarta.xml.ws;
 	requires org.jvnet.jaxb.plugins.runtime;
 	requires slf4j.api;
 	requires jakarta.annotation;
